50fc313fe981a3ae38e56b5073e2867f45199539
yuanlee <yuanlee@163.com>
2023-11-18 15:35:41 +0800

    chore: Add ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 import App from './App.vue'
 import { setupRouter } from './router'
 import { setupStore } from '@/store'
+import 'element-plus/dist/index.css'
 import './assets/main.less'
 
 function bootstrap() {
   const app = createApp(App)
-
+  app.use(ElementPlus)
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
   setupRouter(app)
   setupStore(app)
   app.mount('#app')
