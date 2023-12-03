e405325a7810b2b0c256de94bc2fe742e46dbad9
yuanlee <yuanlee@163.com>
2023-12-04 02:46:49 +0800

    Done: 调用接口错误提示

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -1,26 +1,35 @@
-import type { AxiosResponse } from 'axios'
-import type { Result } from '#/axios'
+import type {AxiosResponse} from 'axios'
+import type {Result} from '#/axios'
 
 import axios from 'axios'
+import {ElNotification} from "element-plus";
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API,
-  timeout: 1000 * 20
+    baseURL: import.meta.env.VITE_BASE_API,
+    timeout: 1000 * 20
 })
 
 // 添加响应拦截器
 request.interceptors.response.use(
 // @ts-ignore
-  function (response: AxiosResponse<Result>): Result {
-      const data = response.data
-      if (data.code !== 0) {
-          throw new Error(data.message ?? '系统异常')
-      }
-      return data
-  },
-  function (error) {
-    return Promise.reject(error)
-  }
+    function (response: AxiosResponse<Result>): Result {
+        const data = response.data
+        if (data.code !== 0) {
+            ElNotification.error({
+                message: data.message,
+                showClose: false
+            })
+        }
+        return data
+    },
+    function (error) {
+        // console.log('response>error:\n', error)
+        ElNotification.error({
+            message: error.response.data.message,
+            showClose: false
+        })
+        return Promise.reject(error)
+    }
 )
 
 export default request
